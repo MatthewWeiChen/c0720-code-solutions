3777f2f64ebbb66a37db3aa027eabad224b4ec84
MattChen1021 <mattchen1021gmail.com>
2020-09-01 19:47:58 +0000

    made some changes

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -7,5 +7,21 @@
 // combine the words together
 // form a sentence with the combined words
 
+// lowercase the entire string
 // split to make into an array
-//
+// grab each index of array and capitalize first letter.
+// put together all the words in a sentence and return it
+
+const capitalizeWords = string => {
+  const lowerCaseSentence = string.toLowerCase();
+  const sentenceArr = lowerCaseSentence.split(' ');
+  let storeWord = '';
+  let remainingWord = '';
+  for (let i = 0; i < sentenceArr.length; i++) {
+    storeWord += sentenceArr[i].charAt().toUpperCase();
+    for (let j = 1; j < sentenceArr.length; j++) {
+      remainingWord += sentenceArr[j];
+    }
+  }
+  return storeWord + remainingWord;
+};
