68005132652dc62b61635a38124f2c8574f43152
MattChen1021 <mattchen1021gmail.com>
2020-09-01 01:02:07 +0000

    Completed object-manipulation-1, deleted excess files

--- array-manipulation-2/src/ban-methods.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-function banMethods(fn, ...methods) {
-  const banned = new RegExp(`${methods.map(m => '\\.' + m).join('|')}`, 'g');
-  if (banned.test(fn.toString())) {
-    throw new Error(`The following methods are banned for this problem: ${methods.join(', ')}`);
-  }
-}
