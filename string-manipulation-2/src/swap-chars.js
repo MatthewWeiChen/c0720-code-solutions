4d7b837d7c1bea56e6c0df608b26160d266fa6b5
MattChen1021 <mattchen1021gmail.com>
2020-09-02 22:40:05 +0000

    brainstorming on last

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+
+// function that swaps indexes given
+// three parameters - index 1 index 2 string
+// receive string (3rd parameter)
+// string placeholder
+// find character at index 1 store in temp
+// find character at index 2 store in temp
+// put index 1 temp in index 2 original spot
+// vice versa
+
+const swapChars = (firstIndex, secondIndex, string) => {
+  const tempString = '';
+  const holdFirstIndex = string[firstIndex];
+  const holdSecondIndex = string[secondIndex];
+
+};
