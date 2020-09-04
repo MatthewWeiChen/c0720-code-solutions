5d2019e5f91c998d0bfeeede006218d7743414fc
MattChen1021 <mattchen1021gmail.com>
2020-09-04 17:02:34 +0000

    still on chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+
+// array of arrays multidimensional
+// receives size for chunk
+// for loop
+// if condition
+// every iteration create it's own array and push to an array
+
+const chunk = (array, size) => {
+  const nestedArr = [];
+  const insideArr = [];
+  for (let i = 0; i < size; i++) {
+    insideArr.push(array[i]);
+  }
+
+  nestedArr.push(insideArr);
+  return nestedArr;
+};
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-unused-vars */
 
 const dropRight = (array, count) => {
-
+  const finalList = [];
+  for (let i = 0; i < array.length - count; i++) {
+    finalList.push(array[i]);
+  }
+  return finalList;
 };
