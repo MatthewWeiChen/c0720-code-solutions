0aaed8331bc2fc7fbb2fd73da8be5ec455a1d24b
MattChen1021 <mattchen1021gmail.com>
2020-09-05 00:03:43 +0000

    completed node-notes-cli

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -5,31 +5,39 @@
 
 const fs = require('fs');
 const dataJson = require('./data.json');
+if (process.argv[2] === 'read') {
 
-fs.readFile('./data.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  for (data in dataJson.notes) {
-    console.log(`${data}: ${dataJson.notes[data]}`);
-  }
-});
-
-// User can add a note
-// writefile
-// if third argument is create execute this
-// stringify
-
-// if (process.argv[2] === "create") {
-//   fs.writeFile('./data.json', dataJson, (err) => {
-//     if (err) {
-//       console.error(err)
-//       process.exit(1);
-//     }
-//     dataJson.nextId++;
-//     // dataJson.notes = JSON.stringify(, null, 2);
-//     // dataJson.notes = process.argv[3];
-//   });
-
-// }
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    for (data in dataJson.notes) {
+      console.log(`${data}: ${dataJson.notes[data]}`);
+    }
+  });
+} else if (process.argv[2] === 'create') {
+  dataJson.notes[dataJson.nextId++] = process.argv[3];
+  fs.writeFile('./data.json', JSON.stringify(dataJson), err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+} else if (process.argv[2] === 'delete') {
+  delete dataJson.notes[`${process.argv[3]}`];
+  fs.writeFile('./data.json', JSON.stringify(dataJson), err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+} else if (process.argv[2] === 'update') {
+  dataJson.notes[`${process.argv[3]}`] = process.argv[4];
+  fs.writeFile('./data.json', JSON.stringify(dataJson), err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+}
