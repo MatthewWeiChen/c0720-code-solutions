a5477325f45aac3a741cdc9d8d02e88fdd7eafc5
MattChen1021 <mattchen1021gmail.com>
2020-09-06 00:00:06 +0000

    completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,18 +1,14 @@
 /* eslint-disable no-unused-vars */
 
-// array of arrays multidimensional
-// receives size for chunk
-// for loop
-// if condition
-// every iteration create it's own array and push to an array
-
 const chunk = (array, size) => {
-  const nestedArr = [];
-  const insideArr = [];
-  for (let i = 0; i < size; i++) {
-    insideArr.push(array[i]);
+  const chunkedList = [];
+  for (let i = 0; i < array.length; i++) {
+    const last = chunkedList[chunkedList.length - 1];
+    if (!last || last.length === size) {
+      chunkedList.push([array[i]]);
+    } else {
+      last.push(array[i]);
+    }
   }
-
-  nestedArr.push(insideArr);
-  return nestedArr;
+  return chunkedList;
 };
