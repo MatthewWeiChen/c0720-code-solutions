493b920ebcaeaba0e71597ed982f0e9801476b15
MattChen1021 <mattchen1021gmail.com>
2020-09-07 23:49:06 +0000

    Completed Object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,23 +1,18 @@
 /* eslint-disable no-unused-vars */
-// input target, source both objects
-// output is a modified target object
-// compares properties in target and source
-// for every property not in target add to target
 
 const defaults = (target, source) => {
   let property;
   const sourceKeys = Object.keys(source);
-  if (Object.keys(target).length === 0) {
+  const targetKeys = Object.keys(target);
+  if (targetKeys.length === 0) {
     Object.assign(target, source);
     return target;
   } else {
-    for (property in target) {
-      if (!sourceKeys.includes(property)) {
+    for (property in source) {
+      if (!targetKeys.includes(property)) {
         target[property] = source[property];
       }
     }
   }
   return target;
 };
-
-// comparing two objects
