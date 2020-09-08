f7d847be436f6484dc2ed81a5e82c06f5fb6b0f3
MattChen1021 <mattchen1021gmail.com>
2020-09-08 22:51:07 +0000

    removed console.log

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(function (req, res) {
-  // eslint-disable-next-line no-console
-  console.log(req);
   res.send('Hello World!');
 });
 
