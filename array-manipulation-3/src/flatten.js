83c3a4835cee57f6a7c767b02a92b064b12fab9f
MattChen1021 <mattchen1021gmail.com>
2020-09-08 19:30:49 +0000

    pseudocode

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -5,3 +5,10 @@
 // flatten array by one level - take out nested elements by 1 array.
 
 // isArray to check for nested arrays.
+// create new array and push elements in that array
+// if its nested go into array and push individual elements out.
+
+const flatten = array => {
+  const flattenedArr = [];
+
+};
