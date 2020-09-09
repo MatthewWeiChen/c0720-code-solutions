10a262e439ae8c3c71a08948263304142faf505a
MattChen1021 <mattchen1021gmail.com>
2020-09-09 16:39:56 +0000

    finished difference

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+// input two arrays
+// output new array
+// difference between first and second
+
+const difference = (first, second) => {
+  const differenceList = [];
+  for (let i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
+      differenceList.push(first[i]);
+    }
+  }
+  for (let i = 0; i < second.length; i++) {
+    if (!first.includes(second[i])) {
+      differenceList.push(second[i]);
+    }
+  }
+  return differenceList;
+};
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,25 @@
 /* eslint-disable no-unused-vars */
+// input array
+// output new array only with unique elements of array
+// order they first appear in array
+// only 1 iteration of each element
+
+// iterate through array
+// each iteration except first check if it is already in array
+// push first and push single copies
+// if copy found ignore it
+
+// includes method
+
+const unique = array => {
+  const uniqueList = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!uniqueList.includes(array[i])) {
+      uniqueList.push(array[i]);
+    }
+  }
+  return uniqueList;
+};
+
+// maybe counter
+// go through each index if current value - index = 0; add
