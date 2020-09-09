ea515434261410366cbe75c22f926e07e7ed41f3
MattChen1021 <mattchen1021gmail.com>
2020-09-09 16:42:26 +0000

    finished intersection

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+// input two arrays
+// output new array
+// new array containing unique values found in both first and second
+// order matters
+
+const intersection = (first, second) => {
+  const intersectionList = [];
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      intersectionList.push(first[i]);
+    }
+  }
+  return intersectionList;
+};
