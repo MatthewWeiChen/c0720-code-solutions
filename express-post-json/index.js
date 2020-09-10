9ebdd51de22830edbfc57b645b1a626c665214cb
MattChen1021 <mattchen1021gmail.com>
2020-09-10 00:10:21 +0000

    completed express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const grades = [];
+const app = express();
+let nextId = 1;
+const parseJson = express.json();
+
+app.use(parseJson);
+
+app.post('/api/grades', (req, res) => {
+  const jsonBody = req.body;
+  res.status(201);
+  if (res.status(201)) {
+    jsonBody.id = nextId++;
+  }
+  grades.push(jsonBody);
+  res.json(grades);
+});
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening to port 3000!');
+});
