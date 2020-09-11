b044dd4a38c45ef6e7b0c612b5188a55349d3509
MattChen1021 <mattchen1021gmail.com>
2020-09-11 01:40:57 +0000

    client can post a new note

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const dataJson = require('./data.json');
 const app = express();
+const parseJson = express.json();
+const fs = require('fs');
 
-// clients can get a list of notes
-// route /api/notes
-// 200 response
-// res.json(json object)
-// require data.json auto parses
+app.use(parseJson);
 
 app.get('/api/notes', (req, res) => {
   res.json(dataJson);
 });
-
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('Listening to port 3000!');
-});
-
-// clients can get a single note by id
-// route /api/notes/:id
-// not positive integer -> 400 response with json object containing error property with problem
-// 200 response json object representing the note with id, don't send out list
-// 404 json obj with error of problem
 
 app.get('/api/notes/:id', (req, res) => {
   const id = parseInt(req.params.id);
@@ -39,4 +26,35 @@ app.get('/api/notes/:id', (req, res) => {
     });
   }
 
+});
+
+app.post('/api/notes', (req, res) => {
+
+  if (Object.keys(req.body).length === 0) {
+    res.status(400);
+    res.json({
+      error: 'content is a required field'
+    });
+  } else {
+    dataJson.notes[`${dataJson.nextId}`] = {
+      id: `${dataJson.nextId}`,
+      content: Object.values(req.body).toString()
+    };
+    dataJson.nextId++;
+    fs.writeFile('derp/data.json', JSON.stringify(dataJson), err => {
+      if (err) {
+        res.status(500).json({
+          error: 'An unexpected error has occured'
+        });
+      } else {
+        res.status(201).json(dataJson.notes[`${dataJson.nextId}`]);
+      }
+    });
+  }
+
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening to port 3000!');
 });
