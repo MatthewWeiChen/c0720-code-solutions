33e05915ff3a2799aadeeb1481236455df961734
MattChen1021 <mattchen1021gmail.com>
2020-09-11 18:27:19 +0000

    changed if and else to if and return

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -17,96 +17,120 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (req.params.id < 0) {
+  const id = parseInt(req.params.id, 10);
+
+  if (req.params.id < 0 || isNaN(req.params.id)) {
     res.status(400);
     res.json({
       error: 'id must be a postive integer'
     });
-  } else if (dataJson.notes[id]) {
-    res.json(dataJson.notes[id]);
-  } else {
-    res.status(404);
-    res.json({
-      error: `cannot find note with id ${req.params.id}`
-    });
+    return;
   }
+
+  if (dataJson.notes[id]) {
+    res.json(dataJson.notes[id]);
+    return;
+  }
+  res.status(404);
+  res.json({
+    error: `cannot find note with id ${id}`
+  });
 });
 
 app.post('/api/notes', (req, res) => {
   const jsonBody = req.body;
+
   if (Object.keys(jsonBody).length === 0) {
     res.status(400).json({
       error: 'content is a required field'
     });
-  } else {
-    jsonBody.id = dataJson.nextId;
-    dataJson.notes[dataJson.nextId] = req.body;
-    dataJson.nextId++;
-    fs.writeFile('data.json', JSON.stringify(dataJson), err => {
-      if (err) {
-        res.status(500).json({
-          error: 'An unexpected error has occured'
-        });
-      } else {
-        res.status(201).json(jsonBody);
-      }
-    });
+    return;
   }
+
+  jsonBody.id = dataJson.nextId;
+  dataJson.notes[dataJson.nextId] = req.body;
+  dataJson.nextId++;
+
+  fs.writeFile('data.json', JSON.stringify(dataJson, null, 2), err => {
+    if (err) {
+      res.status(500).json({
+        error: 'An unexpected error has occured'
+      });
+    } else {
+      res.status(201).json(jsonBody);
+    }
+  });
+
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id, 10);
+
   if (id < 0 || isNaN(req.params.id)) {
     res.status(400).send({
       error: 'id must be a positive integer'
     });
-  } else if (dataJson.notes[id] === undefined) {
+    return;
+  }
+
+  if (dataJson.notes[id] === undefined) {
     res.status(404).json({
       error: `cannot find note with id ${req.params.id}`
     });
-  } else {
-    delete dataJson.notes[id];
-    fs.writeFile('data.json', JSON.stringify(dataJson), err => {
-      if (err) {
-        res.status(500).json({
-          error: 'An unexpected error has occured'
-        });
-      } else {
-        res.status(204).send();
-      }
-    });
+    return;
   }
+
+  delete dataJson.notes[id];
+  fs.writeFile('data.json', JSON.stringify(dataJson), err => {
+    if (err) {
+      res.status(500).json({
+        error: 'An unexpected error has occured'
+      });
+    } else {
+      res.status(204).send();
+    }
+  });
+
 });
 
 app.put('/api/notes/:id', (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id, 10);
   const jsonBody = req.body;
+
   if (id < 0 || isNaN(req.params.id)) {
     res.status(400).send({
       error: 'id must be a positive integer'
     });
-  } else if (Object.keys(jsonBody).length === 0) {
+    return;
+  }
+
+  if (Object.keys(jsonBody).length === 0) {
     res.status(400).json({
       error: 'content is a required field'
     });
-  } else if (dataJson.notes[id] === undefined) {
+    return;
+  }
+
+  if (dataJson.notes[id] === undefined) {
     res.status(404).json({
       error: `cannot find note with id ${req.params.id}`
     });
-  } else {
-    jsonBody.nextId = id;
-    dataJson.notes[id] = jsonBody;
-    fs.writeFile('data.json', JSON.stringify(dataJson), err => {
-      if (err) {
-        res.status(500).json({
-          error: 'An unexpected error has occured'
-        });
-      } else {
-        res.status(200).json(jsonBody);
-      }
-    });
+    return;
   }
+
+  jsonBody.nextId = id;
+  dataJson.notes[id] = jsonBody;
+
+  fs.writeFile('data.json', JSON.stringify(dataJson), err => {
+    if (err) {
+      res.status(500).json({
+        error: 'An unexpected error has occured'
+      });
+    } else {
+      res.status(200).json(jsonBody);
+    }
+  });
+
 });
 
 app.listen(3000, () => {
