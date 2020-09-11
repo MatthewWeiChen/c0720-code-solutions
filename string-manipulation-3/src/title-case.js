718e0ed1a6b4fe550ab7832971f5c87dfe642258
MattChen1021 <mattchen1021gmail.com>
2020-09-11 20:20:16 +0000

    added psuedocode

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,3 +1,8 @@
 /* eslint-disable no-unused-vars */
 // input string
 // output string with APA title case style
+
+// capitalize first word of the title and any subtitle
+// capitalize major words and hyphenated major words
+// capitalize all words of four letters or more
+// lowercase minor words
