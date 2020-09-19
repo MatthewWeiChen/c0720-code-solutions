fd71a6ba8e2ef176fd85c23ba5a2b3573a6071d6
MattChen1021 <mattchen1021gmail.com>
2020-09-19 00:21:32 +0000

    completed fetch-in-react

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({
+        isLoading: false,
+        users: users
+      }))
+      .catch(err =>
+        console.error(err)
+      );
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
---
+++ fetch-in-react/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, 'src');
+const publicPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    contentBase: publicPath,
+    watchContentBase: true,
+    stats: 'minimal'
+  }
+};
