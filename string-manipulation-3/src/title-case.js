7c3150873a157f8e8396f7cc942bf9ac5630e1c0
MattChen1021 <mattchen1021gmail.com>
2020-09-22 08:03:08 +0000

    split by (':')

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -30,8 +30,26 @@ const titleCase = title => {
   const allLowerCase = title.toLowerCase();
   const titleArr = allLowerCase.split(' ');
   const answer = [];
+  const split = allLowerCase.split(':');
+  const secondHalf = split[1];
+  // if (allLowerCase.includes(':')) {
+
+  //   // for (let i = 0; i < titleArr.length; i++) {
+  //   //   if (titleArr[i].endsWith(':')) {
+  //   //     const word = titleArr[i].slice(0, 10);
+  //   //     const indexColon = titleArr[i].indexOf(':');
+  //   //     const colon = titleArr[i].slice(-1);
+  //   //     if (word === 'javascript') {
+  //   //       answer.push(`JavaScript${colon}`);
+  //   //     }
+  //   //   }
+  //   // }
+  //   // for(let j =0; j < secondHalf.length; j++){
+
+  //   // }
+  // } else {
   for (let i = 0; i < titleArr.length; i++) {
-    if (titleArr[i].length >= 4 && !specialWords.includes(titleArr[i])) {
+    if (titleArr[i].length >= 4 && !specialWords.includes(titleArr[i]) && !titleArr[i].endsWith(':')) {
       const upperLetter = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1);
       answer.push(upperLetter);
     } else if (titleArr[i].length < 4 && !minorWords.includes(titleArr[i]) && !specialWords.includes(titleArr[i])) {
@@ -44,8 +62,8 @@ const titleCase = title => {
     } else if (titleArr[i] === 'api') {
       answer.push('API');
     }
-    // if element before current index has ":" capitalize
-    // i recommend splitting the title by ': ' then operating on the first half and the second half
   }
+  // if element before current index has ":" capitalize
+  // i recommend splitting the title by ': ' then operating on the first half and the second half
   return answer.join(' ');
 };
