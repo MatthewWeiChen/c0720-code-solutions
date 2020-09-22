78c2594ee670c39d2b6727201a69bd74c59b0750
MattChen1021 <mattchen1021gmail.com>
2020-09-22 09:00:22 +0000

    made object of images into array

--- react-carousel-component/src/components/carousel.jsx
+++ react-carousel-component/src/components/carousel.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 // import Images from './images';
 
-const images = [
-  {
-    id: 1,
-    link: 'https://pm1.narvii.com/6381/46fefa526ad53600a1440d443bdcf4f6f6e6b866_hq.jpg'
-  },
-  {
-    id: 2,
-    link: 'https://i6.imggur.net/worst/57/worst-605674.jpg'
-  },
-  {
-    id: 3,
-    link: 'https://somewhatwishfulthinking.files.wordpress.com/2015/01/0130-003.jpg'
-  },
-  {
-    id: 4,
-    link: 'https://i6.mangareader.net/worst/107/worst-2296981.jpg'
-  }
-];
+const images =
+  [
+    'https://pm1.narvii.com/6381/46fefa526ad53600a1440d443bdcf4f6f6e6b866_hq.jpg',
+    'https://i6.imggur.net/worst/57/worst-605674.jpg',
+    'https://somewhatwishfulthinking.files.wordpress.com/2015/01/0130-003.jpg',
+    'https://i6.mangareader.net/worst/107/worst-2296981.jpg'
+  ];
 
 export default class Carousel extends React.Component {
   constructor(props) {
