33a1be7d8aa1efa3e402d6ce4c6861ea00462ce8
MattChen1021 <mattchen1021gmail.com>
2020-09-23 06:05:43 +0000

    changed index to display image

--- react-carousel-component/src/components/carousel.jsx
+++ react-carousel-component/src/components/carousel.jsx
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from 'react';
 // import Images from './images';
 
-const images =
-  [
-    'https://pm1.narvii.com/6381/46fefa526ad53600a1440d443bdcf4f6f6e6b866_hq.jpg',
-    'https://i6.imggur.net/worst/57/worst-605674.jpg',
-    'https://somewhatwishfulthinking.files.wordpress.com/2015/01/0130-003.jpg',
-    'https://i6.mangareader.net/worst/107/worst-2296981.jpg'
-  ];
-
-const Carousel = () => {
+const Carousel = props => {
   const [current, setCurrent] = useState(0);
-  const imagesLength = images.length;
+  const imagesLength = props.images.length;
+
   const goNextImage = () => {
     setCurrent(current === imagesLength - 1 ? 0 : current + 1);
   };
@@ -26,7 +19,7 @@ const Carousel = () => {
         <div className="arrow-container">
           <i className="fas fa-angle-left fa-3x"></i>
         </div>
-        <img src='https://pm1.narvii.com/6381/46fefa526ad53600a1440d443bdcf4f6f6e6b866_hq.jpg' alt="worst" className="img-height" />
+        <img src={props.images[0]} alt="worst" className="img-height" />
         <div className="arrow-container">
           <i className="fas fa-angle-right fa-3x"></i>
         </div>
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './components/carousel';
 
-ReactDOM.render(<Carousel />, document.querySelector('#root'));
+const images =
+  [
+    'https://i6.imggur.net/worst/57/worst-605674.jpg',
+    'https://somewhatwishfulthinking.files.wordpress.com/2015/01/0130-003.jpg',
+    'https://i6.mangareader.net/worst/107/worst-2296981.jpg',
+    'https://pm1.narvii.com/6381/46fefa526ad53600a1440d443bdcf4f6f6e6b866_hq.jpg'
+  ];
+
+ReactDOM.render(<Carousel images={images} />, document.querySelector('#root'));
