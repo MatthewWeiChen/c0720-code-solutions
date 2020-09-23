29e6560aa6ce273c405e14ca6607bfdc0147327b
MattChen1021 <mattchen1021gmail.com>
2020-09-23 02:43:42 +0000

    completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -21,49 +21,58 @@ const minorWords = [
 
 const specialWords = ['javascript', 'api'];
 
-// if 4 or greater x
-// between hyphens
-// capitalize first word of the title
-// capitalize major words
-
 const titleCase = title => {
   const allLowerCase = title.toLowerCase();
   const titleArr = allLowerCase.split(' ');
   const answer = [];
-  const split = allLowerCase.split(':');
-  const secondHalf = split[1];
-  // if (allLowerCase.includes(':')) {
-
-  //   // for (let i = 0; i < titleArr.length; i++) {
-  //   //   if (titleArr[i].endsWith(':')) {
-  //   //     const word = titleArr[i].slice(0, 10);
-  //   //     const indexColon = titleArr[i].indexOf(':');
-  //   //     const colon = titleArr[i].slice(-1);
-  //   //     if (word === 'javascript') {
-  //   //       answer.push(`JavaScript${colon}`);
-  //   //     }
-  //   //   }
-  //   // }
-  //   // for(let j =0; j < secondHalf.length; j++){
-
-  //   // }
-  // } else {
-  for (let i = 0; i < titleArr.length; i++) {
-    if (titleArr[i].length >= 4 && !specialWords.includes(titleArr[i]) && !titleArr[i].endsWith(':')) {
-      const upperLetter = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1);
-      answer.push(upperLetter);
-    } else if (titleArr[i].length < 4 && !minorWords.includes(titleArr[i]) && !specialWords.includes(titleArr[i])) {
-      const upperLetter = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1);
-      answer.push(upperLetter);
-    } else if (minorWords.includes(titleArr[i])) {
-      answer.push(titleArr[i]);
-    } else if (titleArr[i] === 'javascript') {
-      answer.push('JavaScript');
-    } else if (titleArr[i] === 'api') {
-      answer.push('API');
+  if (!allLowerCase.includes(':')) {
+    for (let i = 0; i < titleArr.length; i++) {
+      if (titleArr[i].length >= 4 && !specialWords.includes(titleArr[i]) && !titleArr[i].endsWith(':')) {
+        const upperLetter = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1);
+        answer.push(upperLetter);
+      } else if (titleArr[i].length < 4 && !minorWords.includes(titleArr[i]) && !specialWords.includes(titleArr[i])) {
+        const upperLetter = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1);
+        answer.push(upperLetter);
+      } else if (minorWords.includes(titleArr[i])) {
+        answer.push(titleArr[i]);
+      } else if (titleArr[i] === 'javascript') {
+        answer.push('JavaScript');
+      } else if (titleArr[i] === 'api') {
+        answer.push('API');
+      }
     }
+    return answer.join(' ');
+  } else {
+    const splitWords = allLowerCase.split(':');
+    const firstHalf = splitWords[0];
+    const secondHalf = splitWords[1];
+    const firstHalfArr = firstHalf.split(' ');
+    const secondHalfArr = secondHalf.split(' ');
+    secondHalfArr.shift();
+    for (let j = 0; j < firstHalfArr.length; j++) {
+      if (firstHalfArr[j] === 'javascript') {
+        answer.push('JavaScript:');
+      } else {
+        const upperLetter = firstHalfArr[j].charAt(0).toUpperCase() + firstHalfArr[j].slice(1);
+        answer.push(upperLetter);
+      }
+    }
+    for (let k = 0; k < secondHalfArr.length; k++) {
+      if (secondHalfArr[k] === secondHalfArr[0]) {
+        const upperLetter = secondHalfArr[k].charAt(0).toUpperCase() + secondHalfArr[k].slice(1);
+        answer.push(upperLetter);
+      } else if (secondHalfArr[k].includes('-')) {
+        const hyphenWord = secondHalfArr[k].split('-');
+        const upperFirst = hyphenWord[0].charAt().toUpperCase() + hyphenWord[0].slice(1);
+        const upperSecond = hyphenWord[1].charAt().toUpperCase() + hyphenWord[1].slice(1);
+        answer.push(`${upperFirst}-${upperSecond}`);
+      } else if (secondHalfArr[k].length >= 4) {
+        const upperLetter = secondHalfArr[k].charAt(0).toUpperCase() + secondHalfArr[k].slice(1);
+        answer.push(upperLetter);
+      } else {
+        answer.push(secondHalfArr[k]);
+      }
+    }
+    return answer.join(' ');
   }
-  // if element before current index has ":" capitalize
-  // i recommend splitting the title by ': ' then operating on the first half and the second half
-  return answer.join(' ');
 };
