9ea735bd0f6db0a8326c20d9819157711f3c5d36
MattChen1021 <mattchen1021gmail.com>
2020-10-01 21:07:48 +0000

    added arrow functionality

--- react-carousel-component/src/components/carousel.jsx
+++ react-carousel-component/src/components/carousel.jsx
@@ -9,32 +9,41 @@ const Carousel = ({ images }) => {
   };
 
   useEffect(() => {
-    setTimeout(goNextImage, 3000);
-  });
+    const id = setTimeout(goNextImage, 3000);
+    return () => clearTimeout(id);
+  }, [current]);
+
+  const moveRight = () => {
+    setCurrent(current === imagesLength - 1 ? 0 : current + 1);
+  };
+
+  const moveLeft = () => {
+    setCurrent(current === 0 ? 3 : current - 1);
+  };
+
+  // dots hide or show depending on index
 
   return (
     <>
       <div className="container">
-        <div className="arrow-container">
-          <i className="fas fa-angle-left fa-3x"></i>
+        <div className="row d-flex justify-content-center align-items-center">
+          <i onClick={moveLeft} className="fas fa-angle-left fa-3x mr-5"></i>
+          <div>
+            {images.map((image, index) => (
+              <div className={index === current ? 'show' : 'hidden'} key={index}>
+                <img className="img-height" src={image} alt="worst" />
+              </div>
+            ))}
+          </div>
+          <i onClick={moveRight} className="fas fa-angle-right fa-3x ml-5"></i>
         </div>
-        <div>
-          {images.map((image, index) => (
-            <div className={index === current ? 'show' : 'hidden'} key={index}>
-              <img className="img-height" src={image} alt="worst" />
-            </div>
-          ))}
+        <div className="dot-container">
+          <i className="fas fa-circle dot-spacing"></i>
+          <i className="far fa-circle dot-spacing"></i>
+          <i className="far fa-circle dot-spacing"></i>
+          <i className="far fa-circle dot-spacing"></i>
         </div>
-        <div className="arrow-container">
-          <i className="fas fa-angle-right fa-3x"></i>
-        </div>
-      </div>
-      <div className="dot-container">
-        <i className="fas fa-circle dot-spacing"></i>
-        <i className="far fa-circle dot-spacing"></i>
-        <i className="far fa-circle dot-spacing"></i>
-        <i className="far fa-circle dot-spacing"></i>
-        <i className="far fa-circle dot-spacing"></i>
+
       </div>
     </>
   );
